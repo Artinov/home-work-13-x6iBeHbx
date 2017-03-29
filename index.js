7eb329ae568c18f9ff4be272180e8dc3277e84c8
x6iBeHbx <xxxaavxxx@gmail.com>
2017-03-29 18:47:43 +0300

    hm13

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
+var math = require('mathjs');
+
 var http = require('http');
 var port = 3000;
 
-var server = http.createServer(function(request, response) {
-    switch (request.url) {
-        case '/':
-            response.end("Hello");
-            break;
-    }
+var server = http.createServer(function(request, response){
+	var formula = request.url.substr(1);
+	console.log(formula.toString());
+	var result = math.eval(formula.toString());
+	console.log(result);
+	response.end(result.toString());
 });
 
-server.listen(port, function() {
-    console.log("Server is listening port: " + port);
-    console.log("Visit http://localhost:" + port + "to see your app");
-})
+server.listen(port, function(){
+	console.log("Srever is listening port: " + port);
+});
